de24b75f6985c7008f5c9dbf3cbf92e482431335
Vladimirs Anufrijevs <vladimirs.anufrijevs@gmail.com>
2024-11-20 23:05:44 +0200

    Add 1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
+
+        int yearsOld = 18;
+        if (yearsOld >= 18) {
+            System.out.println("Если возраст человека равен " + yearsOld +", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + yearsOld + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
